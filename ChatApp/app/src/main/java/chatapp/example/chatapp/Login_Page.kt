f6f370f292694a1b5a57040d92ca14b513428128
prathamkumar1812 <prathamkumar.mbd@gmail.com>
2023-02-08 00:45:22 +0530

    Create Login Page

---
+++ ChatApp/app/src/main/java/chatapp/example/chatapp/Login_Page.kt
@@ -0,0 +1,11 @@
+package chatapp.example.chatapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Login_Page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_page)
+    }
+}
